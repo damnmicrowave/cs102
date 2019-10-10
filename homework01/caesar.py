26c2eda080ddda47a64c58381c9ce5479e4afe08
Alex <damnmicrowave@gmail.com>
2019-10-10 16:04:28 +0300

    Optimized caesar code

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -3,9 +3,9 @@ def encrypt_caesar(plaintext: str, shift: int) -> str:
     for char in plaintext:
         code = ord(char)
         if code in range(65, 91):
-            code = code + shift - 26 if code + shift > 90 else code + shift
+            code += shift - 26 if code + shift > 90 else shift
         elif code in range(97, 123):
-            code = code + shift - 26 if code + shift > 122 else code + shift
+            code += shift - 26 if code + shift > 122 else shift
         ciphertext += chr(code)
     return ciphertext
 
@@ -15,8 +15,8 @@ def decrypt_caesar(ciphertext: str, shift: int) -> str:
     for char in ciphertext:
         code = ord(char)
         if code in range(65, 91):
-            code = code - shift + 26 if code - shift < 65 else code - shift
+            code -= shift - 26 if code - shift < 65 else shift
         elif code in range(97, 123):
-            code = code - shift + 26 if code - shift < 97 else code - shift
+            code -= shift - 26 if code - shift < 97 else shift
         plaintext += chr(code)
     return plaintext
