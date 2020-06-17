00ce18bc650a253cd8986965624987cdbd2d4d24
Alex <dammicrowave@gmail.com>
2020-06-17 20:43:04 +0300

    Parsed new & added database

--- homework06/db.py
+++ homework06/db.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from scraputils import get_news
 
 Base = declarative_base()
-engine = create_engine("sqlite:///news.db")
+engine = create_engine("sqlite:///hackernews.db")
 session = sessionmaker(bind=engine)
 
 
@@ -15,8 +16,27 @@ class News(Base):
     title = Column(String)
     author = Column(String)
     url = Column(String)
+    domain = Column(String)
     comments = Column(Integer)
     points = Column(Integer)
     label = Column(String)
 
+
 Base.metadata.create_all(bind=engine)
+
+
+if __name__ == '__main__':
+    s = session()
+
+    for n in get_news('https://news.ycombinator.com/newest', 34):
+        news_item = News(
+            title=n['title'],
+            author=n['author'],
+            url=n['url'],
+            domain=n['domain'],
+            comments=n['comments'],
+            points=n['points'],
+        )
+        s.add(news_item)
+
+    s.commit()
