688d7fe114b04dcf78a9334c905d0f80c1fab4d9
Alex <dammicrowave@gmail.com>
2020-06-17 22:31:09 +0300

    Added classificator

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,16 +1,74 @@
+import math
+
+
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha):
-        pass
+    def __init__(self, alpha=1):
+        self.a = alpha
+        self.word_dict = dict()
+        self.class_inds = dict()
+        self.y = None
+        self.doc_prob = None
+        self.n_classes = None
 
-    def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
-        pass
+    def fit(self, x, y):
+        self.y = list(set(y))
+        words = []
+        for doc in x:
+            words.extend(doc.split(' '))
+        d = len(set(words))
+        for c in y:
+            if c not in self.class_inds.keys():
+                self.class_inds[c] = len(self.class_inds) - 1
+        self.n_classes = len(self.class_inds)
+        for i, doc in enumerate(x):
+            for word in doc.split():
+                if word not in self.word_dict.keys():
+                    self.word_dict[word] = [0 for _ in range(self.n_classes)]
+                self.word_dict[word][self.class_inds[y[i]]] += 1
+        for word in self.word_dict.keys():
+            n_word = sum(self.word_dict[word])
+            for i, n_word_in_class in enumerate(self.word_dict[word]):
+                self.word_dict[word][i] = math.log((n_word_in_class + self.a) / (n_word + self.a * d), math.e)
 
-    def predict(self, X):
-        """ Perform classification on an array of test vectors X. """
-        pass
+    def predict(self, x):
+        self.doc_prob = list()
+        for doc in x:
+            probs = list()
+            for c in self.class_inds.keys():
+                probs.append(math.log(1 / self.n_classes, math.e) + sum([
+                    self.word_dict[word][self.class_inds[c]]
+                    if word in self.word_dict.keys()
+                    else 0
+                    for word in doc.split()
+                ]))
+            self.doc_prob.append(probs.index(max(probs)))
+        return [self.y[c] for c in self.doc_prob]
 
-    def score(self, X_test, y_test):
-        """ Returns the mean accuracy on the given test data and labels. """
-        pass
+    def score(self, x_test, y_test):
+        prediction = self.predict(x_test)
+        c = sum([1 for i, p in enumerate(prediction) if p == y_test[i]])
+        return c / len(prediction)
+
+
+if __name__ == '__main__':
+    import csv
+    import string
+
+    with open("data/SMSSpamCollection") as f:
+        data = list(csv.reader(f, delimiter="\t"))
+
+    def clean(s):
+        translator = str.maketrans("", "", string.punctuation)
+        return s.translate(translator)
+
+
+    spam_x, spam_y = [], []
+    for target, msg in data:
+        spam_x.append(msg)
+        spam_y.append(target)
+    spam_x = [clean(x).lower() for x in spam_x]
+    x_train, y_train, spam_x_test, spam_y_test = spam_x[:3900], spam_y[:3900], spam_x[3900:], spam_y[3900:]
+    model = NaiveBayesClassifier()
+    model.fit(x_train, y_train)
+    print(model.score(spam_x_test, spam_y_test))
