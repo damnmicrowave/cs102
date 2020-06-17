20e2472dad05ea14da7dd0d558d56148fc37d2ae
Alex <dammicrowave@gmail.com>
2020-06-17 22:40:33 +0300

    Added news classification

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,3 +1,5 @@
+import string
+
 from bottle import (
     route, run, template, request, redirect
 )
@@ -5,6 +7,9 @@ from bottle import (
 from scraputils import get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
+
+
+model = NaiveBayesClassifier()
 
 
 @route("/news")
@@ -50,9 +55,27 @@ def update_news():
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
-    pass
+    s = session()
+    news = s.query(News).filter(News.label.is_(None)).all()
+    news_titles = [news_item.title for news_item in news]
+    news_urls = [news_item.url for news_item in news]
+    news = list(zip(news_titles, news_urls))
+    good = [f"{news[i][0]} {news[i][1]}" for i, p in enumerate(model.predict(news_titles)) if p == 'good']
+    maybe = [news_titles[i] for i, p in enumerate(model.predict(news_titles)) if p == 'maybe']
+    never = [news_titles[i] for i, p in enumerate(model.predict(news_titles)) if p == 'never']
+    print(good, maybe, never)
+    return template('news_recommendations', rows=good)
 
 
 if __name__ == "__main__":
+    x, y = [], []
+
+    def clean(title):
+        translator = str.maketrans("", "", string.punctuation)
+        return title.translate(translator)
+
+    for n in session().query(News).all():
+        x.append(clean(n.title).lower())
+        y.append(n.label)
+    model.fit(x, y)
     run(host="localhost", port=8080)
